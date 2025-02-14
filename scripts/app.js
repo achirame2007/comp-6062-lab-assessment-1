330dca5270755f9ff506d4b647aaffaddb4d454a
achirame2007 <atharvachirame2007@gmail.com>
2025-02-13 19:06:32 -0500

    Added html, css and js file

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+const studentFullName = "Atharva Chirame";
+console.log(studentFullName);
+const studentNumber = 1151199;
+console.log(studentNumber);
+const result = `${studentFullName} - ${studentNumber}`;
+console.log(result);
+
+let headerContent = document.querySelector("h1");
+headerContent.classList.add("headingPrimary");
+headerContent.textContent = result;
